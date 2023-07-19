fecaf99d99c07469486ea4caf146a21afed0a4ec
Monika46 <bokoteymonika@hotmail.com>
2023-07-19 14:08:13 +0300

    no message

---
+++ src/main/java/com/ensummerapprenticejava/ensummerapprenticejava/EnSummerApprenticeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.ensummerapprenticejava.ensummerapprenticejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnSummerApprenticeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnSummerApprenticeJavaApplication.class, args);
+	}
+
+}
